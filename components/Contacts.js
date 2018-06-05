d96a838b05898117f1ae1422c41c9a837e7779cf
Joanna Urbiak <joanna.urbiak@o2.pl>
2018-06-05 18:49:21 +0200

    Add files

---
+++ components/Contacts.js
@@ -0,0 +1,21 @@
+var Contact = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+      React.createElement('div', {className: 'contactItem'},
+        React.createElement('img', {
+          className: 'contactImage',
+          src: 'contact.ico'
+        }),
+        React.createElement('p', {className: 'contactLabel'}, 'First Name: ' + this.props.item.firstName),
+        React.createElement('p', {className: 'contactLabel'}, 'Last Name: ' + this.props.item.lastName),
+        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+          this.props.item.email
+        )
+      )
+    )
+  },
+});
